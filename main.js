9389f6f5a6f4ba782916360784bd8d4d7276a6cc
jugabriela <127451876+jugabriela@users.noreply.github.com>
2023-12-08 19:37:09 -0300

    081223
    0812

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 function tocaSom (seletorAudio) {
-    document.querySelector(seletorAudio);
+    const elemento = document.querySelector(seletorAudio);
+
+    
+    if(elemento != null && elemento.localName === 'audio') {
+      elemento.play();
+    }
+    
+  else{
+    console.log('elemento não encontrado ou seletor inválido');
+    
+   }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -15,15 +26,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
-    listaDeTeclas.onkeydown = function (evento)  {
-     
+    tecla.onkeydown = function(evento){
 
-     if (evento.code === 'Space' ||evento.code === 'Enter') {
-        tecla.classList.add('ativa');
-     }
+       
+        if (evento.code === 'Space' ||evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+
 
     }
-
+    
     tecla.onkeyup = function() {
         tecla.classList.remove('ativa');
     }
